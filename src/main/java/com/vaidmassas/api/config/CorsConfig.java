368384ff740575f6fe95e67f4e3046839b541e6a
guidev2026 <guidev2026@gmail.com>
2026-04-19 13:00:29 -0300

    modificando o cors para o deploy

--- src/main/java/com/vaidmassas/api/config/CorsConfig.java
+++ src/main/java/com/vaidmassas/api/config/CorsConfig.java
@@ -10,9 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")  // Porta do seu React/Vite
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://vaidmassas-frontend-git-main-guidev2026s-projects.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .maxAge(3600);
     }
 }
